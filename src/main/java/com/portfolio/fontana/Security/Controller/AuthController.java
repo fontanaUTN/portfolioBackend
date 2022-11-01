f069ff0993a0c877c7870c66e4fe5d793175feef
Federico Fontana <fontana.utn@gmail.com>
2022-10-31 21:49:41 -0300

    add cors url

--- src/main/java/com/portfolio/fontana/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/fontana/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://portfolio-frontend-cc3de.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
